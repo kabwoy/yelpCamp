5d1a25c378768b8de8a9a9f31a1c77e37748975f
Edward Kaboi <deejaykabwoy@gmail.com>
2022-03-17 18:56:24 +0300

    With Full CRUD Functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const methodOverride = require("method-override")
 const Campground = require("./models/campground")
 
 mongoose.connect("mongodb://localhost/yelp-camp").then(()=>{
@@ -12,21 +13,13 @@ const app = express()
 
 app.use(express.urlencoded({extended:true}))
 app.set("view engine" , "ejs")
+app.use(methodOverride('_method'))
 
 app.get("/" , (req,res)=>{
     res.render("home")
 })
 
-app.get('/makecampground' , async(req,res)=>{
-    const newdata = new Campground({
-        title:"My Backyard"
-    })
 
-    await newdata.save()
-
-    res.send(newdata)
-
-})
 
 app.get("/campgrounds/new" , (req , res)=>{
 
@@ -52,9 +45,30 @@ app.post("/campgrounds" , async (req, res)=>{
     res.redirect(`/campgrounds/${newData._id}`)
 })
 
+app.get("/campgrounds/:id/edit" , async (req, res)=>{
 
+    const {id} = req.params
+
+    let foundCamp = await Campground.findById(id)
+
+    res.render("campgrounds/edit" ,{foundCamp})
+
+})
+
+app.put("/campgrounds/:id" , async (req , res)=>{
+    var {id} = req.params
+    let up = await Campground.findByIdAndUpdate(id , req.body , {runValidators:true , new:true})
+    console.log(up)
+    res.redirect(`/campgrounds/${up._id}`)
+})
+
+app.delete("/campgrounds/:id" , async (req, res)=>{
+    var {id} = req.params
+    await Campground.findByIdAndDelete(id)
+    res.redirect("/campgrounds")
+})
 
 app.listen(3000 , ()=>{
-
+    
     console.log("Serving At Port 3000")
 })
